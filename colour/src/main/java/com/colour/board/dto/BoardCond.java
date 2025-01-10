fd74dcabe6ba6ea1d7d9163d3b3c95371c48f947
AidenRoh <colohrby@gmail.com>
2025-01-10 23:19:09 +0900

    feature: add board dto for update and search

---
+++ colour/src/main/java/com/colour/board/dto/BoardCond.java
@@ -0,0 +1,7 @@
+package com.colour.board.dto;
+
+public class BoardCond {
+
+    private String keyword;
+    private Long tagId;
+}
---
+++ colour/src/main/java/com/colour/board/dto/BoardUpdateDto.java
@@ -0,0 +1,13 @@
+package com.colour.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardUpdateDto {
+
+    private String updatedTitle;
+    private String updatedContent;
+
+}
