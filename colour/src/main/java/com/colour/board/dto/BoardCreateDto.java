5a6f912e8363df358c9f156012ffc4fe27fd5b1c
AidenRoh <colohrby@gmail.com>
2025-01-11 01:27:53 +0900

    feature: add dto for board creation

---
+++ colour/src/main/java/com/colour/board/dto/BoardCreateDto.java
@@ -0,0 +1,12 @@
+package com.colour.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardCreateDto {
+
+    private String title;
+    private String content;
+}
