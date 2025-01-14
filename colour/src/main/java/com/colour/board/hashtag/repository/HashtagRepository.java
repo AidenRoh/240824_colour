479dfc5fdb762e7e01d4e3c61dab049774b40d3f
AidenRoh <colohrby@gmail.com>
2025-01-15 02:03:10 +0900

    feature: add hashtag logic

---
+++ colour/src/main/java/com/colour/board/hashtag/repository/HashtagRepository.java
@@ -0,0 +1,20 @@
+package com.colour.board.hashtag.repository;
+
+import com.colour.board.hashtag.dto.HashtagVo;
+import com.colour.board.hashtag.entity.Hashtag;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HashtagRepository {
+
+    Hashtag save(Hashtag hashtag);
+    void tagUp(Long tagId);
+    void tagDown(Long tagId);
+    Optional<Hashtag> findById(Long tagId);
+    Optional<Hashtag> findByTag(String tagName);
+    List<Hashtag> findByCond(HashtagVo cond);
+    void delete(Long tagId);
+}
