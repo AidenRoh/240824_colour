e566e9596d00e9d16a3ab34b8cfa70b6e074ec70
AidenRoh <colohrby@gmail.com>
2025-01-15 02:00:58 +0900

    refactor: combine hashtag and tag post configuration

--- colour/src/main/java/com/colour/board/config/BoardConfig.java
+++ colour/src/main/java/com/colour/board/config/BoardConfig.java
@@ -1,9 +1,17 @@
 package com.colour.board.config;
 
-import com.colour.board.repository.BoardJdbcRepository;
-import com.colour.board.repository.BoardRepository;
-import com.colour.board.service.BoardService;
-import com.colour.board.service.BoardServiceImpl;
+import com.colour.board.hashtag.repository.HashtagRepository;
+import com.colour.board.hashtag.repository.TagJdbcRepository;
+import com.colour.board.hashtag.service.HashtagService;
+import com.colour.board.hashtag.service.HashtagServiceImpl;
+import com.colour.board.post.repository.PostJdbcRepository;
+import com.colour.board.post.repository.PostRepository;
+import com.colour.board.post.service.PostService;
+import com.colour.board.post.service.PostServiceImpl;
+import com.colour.board.tagtopost.repository.TagPostJdbcRepository;
+import com.colour.board.tagtopost.repository.TagPostRepository;
+import com.colour.board.tagtopost.service.TagPostService;
+import com.colour.board.tagtopost.service.TagPostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +25,33 @@ public class BoardConfig {
     private final DataSource dataSource;
 
     @Bean
-    public BoardService boardService() {
-        return new BoardServiceImpl(boardRepository());
+    public PostService postService() {
+        return new PostServiceImpl(postRepository());
     }
 
     @Bean
-    public BoardRepository boardRepository() {
-        return new BoardJdbcRepository(dataSource);
+    public PostRepository postRepository() {
+        return new PostJdbcRepository(dataSource);
     }
+
+    @Bean
+    public HashtagService hashtagService() {
+        return new HashtagServiceImpl(hashtagRepository());
+    }
+
+    @Bean
+    public HashtagRepository hashtagRepository() {
+        return new TagJdbcRepository(dataSource);
+    }
+
+    @Bean
+    public TagPostService tagPostService() {
+        return new TagPostServiceImpl(tagPostRepository());
+    }
+
+    @Bean
+    public TagPostRepository tagPostRepository() {
+        return new TagPostJdbcRepository(dataSource);
+    }
+
 }
