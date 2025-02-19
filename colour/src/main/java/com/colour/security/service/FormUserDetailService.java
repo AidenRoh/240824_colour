797cbe06b8adcbbab5d0bd9298745d89741374f8
AidenRoh <colohrby@gmail.com>
2025-02-19 21:03:27 +0900

    feature: add form login base security

---
+++ colour/src/main/java/com/colour/security/service/FormUserDetailService.java
@@ -0,0 +1,38 @@
+package com.colour.security.service;
+
+import com.colour.member.domain.MemberMapper;
+import com.colour.member.domain.dto.MemberSecurityContext;
+import com.colour.member.domain.dto.MemberSecurityDto;
+import com.colour.member.domain.entity.Member;
+import com.colour.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FormUserDetailService implements UserDetailsService {
+
+    private final MemberRepository repository;
+    private final MemberMapper memberMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member findMember = repository.findByEmail(email);
+        if (findMember == null) {
+            if (repository.existsByEmail(email)) {
+                throw new UsernameNotFoundException("No user found with this email: " + email);
+            }
+        }
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(findMember.getRole()));
+        MemberSecurityDto dto = memberMapper.entityToMemberSecurityDto(findMember);
+        return new MemberSecurityContext(authorities, dto);
+    }
+}
