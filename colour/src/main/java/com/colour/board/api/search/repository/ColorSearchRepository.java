9d21aacf56cb098d6317193ce19253bc0fcb4bbd
AidenRoh <colohrby@gmail.com>
2025-03-16 20:41:34 +0900

    refactor: change dynamic query logic from arraylist to stringbuilder

--- colour/src/main/java/com/colour/board/api/search/repository/ColorSearchRepository.java
+++ colour/src/main/java/com/colour/board/api/search/repository/ColorSearchRepository.java
@@ -6,7 +6,10 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ColorSearchRepository {
@@ -25,17 +28,19 @@ public class ColorSearchRepository {
                 WHERE
                 """);
         Map<String, Object> params = new HashMap<>();
-        List<String> conditions = new ArrayList<>();
+        StringBuilder conditions = new StringBuilder();
         for (int i = 0; i < colors.size(); i++) {
+            if (i > 0) conditions.append(" OR ");
+            conditions.append("(c.hue = :hue").append(i)
+                    .append(" AND c.saturation = :saturation").append(i)
+                    .append(" AND c.lightness = :lightness").append(i).append(")");
+
             HexColor each = colors.get(i);
-            conditions.add("(c.hue = :hue" + i
-                    + " AND c.saturation = :saturation" + i
-                    + " AND c.lightness = :lightness" + i + ")");
             params.put("hue" + i, each.getHue());
             params.put("saturation" + i, each.getSaturation());
             params.put("lightness" + i, each.getLightness());
         }
-        sql.append("(").append(String.join(" OR ", conditions)).append(")").append(" ORDER BY NULL");
+        sql.append("(").append(conditions).append(")").append(" ORDER BY NULL");
 
         return template.queryForList(sql.toString(), params, Long.class);
     }
@@ -48,24 +53,26 @@ public class ColorSearchRepository {
                 WHERE
                 """);
         Map<String, Object> params = new HashMap<>();
-        List<String> whenConditions = new ArrayList<>();
-        List<String> caseConditions = new ArrayList<>();
+        StringBuilder whenConditions = new StringBuilder();
+        StringBuilder caseConditions = new StringBuilder();
         for (int i = 0; i < colors.size(); i++) {
-            HexColor each = colors.get(i);
             String subCondition = "c.hue = :hue" + i
                     + " AND c.saturation = :saturation" + i
                     + " AND c.lightness = :lightness" + i;
-            whenConditions.add("(" + subCondition + ")");
-            caseConditions.add("WHEN " + subCondition + " THEN 1");
+            if (i > 0) whenConditions.append(" OR ");
+            whenConditions.append("(").append(subCondition).append(")");
+            caseConditions.append("WHEN ").append(subCondition).append(" THEN 1\n");
+
+            HexColor each = colors.get(i);
             params.put("hue" + i, each.getHue());
             params.put("saturation" + i, each.getSaturation());
             params.put("lightness" + i, each.getLightness());
         }
-        sql.append("(").append(String.join(" OR ", whenConditions)).append(")").append("""
+        sql.append("(").append(whenConditions).append(")").append("""
                 \nGROUP BY pc.post_id
                 HAVING COUNT(CASE
-                """).append(String.join("\n", caseConditions)).append("""
-                \nEND) = :colorCount
+                """).append(caseConditions).append("""
+                END) = :colorCount
                 ORDER BY NULL
                 """);
         params.put("colorCount", colors.size());
@@ -84,28 +91,31 @@ public class ColorSearchRepository {
                 WHERE
                 """);
         Map<String, Object> params = new HashMap<>();
-        List<String> conditions = new ArrayList<>();
+        StringBuilder conditions = new StringBuilder();
         for (int i = 0; i < conds.size(); i++) {
             ColorCond each = conds.get(i);
-            List<String> subConditions = new ArrayList<>();
+            StringBuilder subConditions = new StringBuilder();
 
             if (each.getHue() != null) {
-                subConditions.add("c.hue = :hue" + i);
+                subConditions.append("c.hue = :hue").append(i);
                 params.put("hue" + i, each.getHue());
             }
             if (each.getSaturation() != null) {
-                subConditions.add("c.saturation = :saturation" + i);
+                if (!subConditions.isEmpty()) subConditions.append(" AND ");
+                subConditions.append("c.saturation = :saturation").append(i);
                 params.put("saturation" + i, each.getSaturation());
             }
             if (each.getLightness() != null) {
-                subConditions.add("c.lightness = :lightness" + i);
+                if (!subConditions.isEmpty()) subConditions.append(" AND ");
+                subConditions.append("c.lightness = :lightness").append(i);
                 params.put("lightness" + i, each.getLightness());
             }
             if (!subConditions.isEmpty()) {
-                conditions.add("(" + String.join(" AND ", subConditions) + ")");
+                if (i > 0) conditions.append(" OR ");
+                conditions.append("(").append(subConditions).append(")");
             }
         }
-        sql.append("(").append(String.join(" OR ", conditions)).append(")").append(" ORDER BY NULL");
+        sql.append("(").append(conditions).append(")").append(" ORDER BY NULL");
 
         return template.queryForList(sql.toString(), params, Long.class);
     }
@@ -118,35 +128,37 @@ public class ColorSearchRepository {
                 WHERE
                 """);
         Map<String, Object> params = new HashMap<>();
-        List<String> whenConditions = new ArrayList<>();
-        List<String> caseConditions = new ArrayList<>();
+        StringBuilder whenConditions = new StringBuilder();
+        StringBuilder caseConditions = new StringBuilder();
         for (int i = 0; i < conds.size(); i++) {
             ColorCond each = conds.get(i);
-            List<String> subConditions = new ArrayList<>();
+            StringBuilder subConditions = new StringBuilder();
 
             if (each.getHue() != null) {
-                subConditions.add("c.hue = :hue" + i);
+                subConditions.append("c.hue = :hue").append(i);
                 params.put("hue" + i, each.getHue());
             }
             if (each.getSaturation() != null) {
-                subConditions.add("c.saturation = :saturation" + i);
+                if (!subConditions.isEmpty()) subConditions.append(" AND ");
+                subConditions.append("c.saturation = :saturation").append(i);
                 params.put("saturation" + i, each.getSaturation());
             }
             if (each.getLightness() != null) {
-                subConditions.add("c.lightness = :lightness" + i);
+                if (!subConditions.isEmpty()) subConditions.append(" AND ");
+                subConditions.append("c.lightness = :lightness").append(i);
                 params.put("lightness" + i, each.getLightness());
             }
             if (!subConditions.isEmpty()) {
-                String subCondition = String.join(" AND ", subConditions);
-                whenConditions.add("(" + subCondition + ")");
-                caseConditions.add("WHEN " + subCondition + " THEN 1");
+                if (i > 0) whenConditions.append(" OR ");
+                whenConditions.append("(").append(subConditions).append(")");
+                caseConditions.append("WHEN ").append(subConditions).append(" THEN 1\n");
             }
         }
-        sql.append("(").append(String.join(" OR ", whenConditions)).append(")").append("""
+        sql.append("(").append(whenConditions).append(")").append("""
                 \nGROUP BY pc.post_id
                 HAVING COUNT(CASE
-                """).append(String.join("\n", caseConditions)).append("""
-                \nEND) = :colorCount
+                """).append(caseConditions).append("""
+                END) = :colorCount
                 ORDER BY NULL
                 """);
         params.put("colorCount", conds.size());
