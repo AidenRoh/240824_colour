985089a7057102cec4b920ed20d103890d63600f
AidenRoh <colohrby@gmail.com>
2025-03-17 22:22:03 +0900

    refactor: integrate and / or logic into one and add color match score dto to contain result

---
+++ colour/src/main/java/com/colour/board/api/search/domain/dto/ColorMatchScore.java
@@ -0,0 +1,11 @@
+package com.colour.board.api.search.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ColorMatchScore {
+    Long postId;
+    Long matchScore;
+}
--- colour/src/main/java/com/colour/board/api/search/repository/ColorSearchRepository.java
+++ colour/src/main/java/com/colour/board/api/search/repository/ColorSearchRepository.java
@@ -2,6 +2,9 @@ package com.colour.board.api.search.repository;
 
 import com.colour.board.api.colorpalette.domain.dto.ColorCond;
 import com.colour.board.api.colorpalette.domain.entity.HexColor;
+import com.colour.board.api.search.domain.dto.ColorMatchScore;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -20,81 +23,72 @@ public class ColorSearchRepository {
         this.template = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public List<Long> searchByColorOR(List<HexColor> colors) {
-        StringBuilder sql = new StringBuilder("""
-                SELECT pc.post_id
-                FROM post_color pc
-                JOIN color c ON pc.color_id = c.color_id
-                WHERE
-                """);
+    public List<ColorMatchScore> monoColorSearchByHex(List<HexColor> colors) {
+        StringBuilder sql = new StringBuilder("WITH ColorFilter AS ( SELECT pc.post_id, ");
         Map<String, Object> params = new HashMap<>();
-        StringBuilder conditions = new StringBuilder();
-        for (int i = 0; i < colors.size(); i++) {
-            if (i > 0) conditions.append(" OR ");
-            conditions.append("(c.hue = :hue").append(i)
-                    .append(" AND c.saturation = :saturation").append(i)
-                    .append(" AND c.lightness = :lightness").append(i).append(")");
-
-            HexColor each = colors.get(i);
-            params.put("hue" + i, each.getHue());
-            params.put("saturation" + i, each.getSaturation());
-            params.put("lightness" + i, each.getLightness());
-        }
-        sql.append("(").append(conditions).append(")").append(" ORDER BY NULL");
-
-        return template.queryForList(sql.toString(), params, Long.class);
-    }
-
-    public List<Long> searchByColorAND(List<HexColor> colors) {
-        StringBuilder sql = new StringBuilder("""
-                SELECT pc.post_id
-                FROM post_color pc
-                JOIN color c ON pc.color_id = c.color_id
-                WHERE
-                """);
-        Map<String, Object> params = new HashMap<>();
-        StringBuilder whenConditions = new StringBuilder();
         StringBuilder caseConditions = new StringBuilder();
+        StringBuilder whereConditions = new StringBuilder();
+        StringBuilder matchScore = new StringBuilder();
+        StringBuilder orderPriority = new StringBuilder();
         for (int i = 0; i < colors.size(); i++) {
             String subCondition = "c.hue = :hue" + i
                     + " AND c.saturation = :saturation" + i
                     + " AND c.lightness = :lightness" + i;
-            if (i > 0) whenConditions.append(" OR ");
-            whenConditions.append("(").append(subCondition).append(")");
-            caseConditions.append("WHEN ").append(subCondition).append(" THEN 1\n");
+            String conditionName = "condition_" + i;
+
+            if (i > 0) {
+                caseConditions.append(", ");
+                whereConditions.append(" OR ");
+                matchScore.append(" + ");
+                orderPriority.append(" + ");
+            }
+            caseConditions.append("SUM(CASE WHEN ").append(subCondition).append(" THEN 1 ELSE 0 END) AS ").append(conditionName);
+            whereConditions.append("(").append(subCondition).append(")");
+            matchScore.append("(CASE WHEN ").append(conditionName).append(" > 0 THEN 1 ELSE 0 END)");
+            orderPriority.append(conditionName);
 
             HexColor each = colors.get(i);
             params.put("hue" + i, each.getHue());
             params.put("saturation" + i, each.getSaturation());
             params.put("lightness" + i, each.getLightness());
         }
-        sql.append("(").append(whenConditions).append(")").append("""
-                \nGROUP BY pc.post_id
-                HAVING COUNT(CASE
-                """).append(caseConditions).append("""
-                END) = :colorCount
-                ORDER BY NULL
-                """);
-        params.put("colorCount", colors.size());
+        sql.append(caseConditions)
+                .append("""
+                        \nFROM post_color pc
+                        JOIN color c ON pc.color_id = c.color_id
+                        WHERE
+                        """)
+                .append("(").append(whereConditions).append(")")
+                .append("""
+                        \nGROUP BY pc.post_id
+                        )
+                        SELECT post_id,
+                        """)
+                .append("(").append(matchScore).append(") AS match_score")
+                .append("""
+                        \nFROM ColorFilter
+                        ORDER BY match_score DESC,
+                        """)
+                .append(orderPriority).append(" DESC");
 
-        return template.queryForList(sql.toString(), params, Long.class);
+        return template.query(sql.toString(), params, colorMatchScoreRowMapper());
     }
 
-    public List<Long> searchByColorCondOR(List<ColorCond> conds) {
+    public List<ColorMatchScore> monoColorSearchByCond(List<ColorCond> conds) {
         if (conds == null || conds.isEmpty()) {
             return Collections.emptyList();
         }
-        StringBuilder sql = new StringBuilder("""
-                SELECT pc.post_id
-                FROM post_color pc
-                JOIN color c ON pc.color_id = c.color_id
-                WHERE
-                """);
+
+        StringBuilder sql = new StringBuilder("WITH ColorFilter AS ( SELECT pc.post_id, ");
         Map<String, Object> params = new HashMap<>();
-        StringBuilder conditions = new StringBuilder();
+        StringBuilder caseConditions = new StringBuilder();
+        StringBuilder whereConditions = new StringBuilder();
+        StringBuilder matchScore = new StringBuilder();
+        StringBuilder orderPriority = new StringBuilder();
         for (int i = 0; i < conds.size(); i++) {
             ColorCond each = conds.get(i);
             StringBuilder subConditions = new StringBuilder();
+            String conditionName = "condition_" + i;
 
             if (each.getHue() != null) {
                 subConditions.append("c.hue = :hue").append(i);
@@ -111,59 +105,46 @@ public class ColorSearchRepository {
                 params.put("lightness" + i, each.getLightness());
             }
             if (!subConditions.isEmpty()) {
-                if (i > 0) conditions.append(" OR ");
-                conditions.append("(").append(subConditions).append(")");
+                if (i > 0) {
+                    caseConditions.append(", ");
+                    whereConditions.append(" OR ");
+                    matchScore.append(" + ");
+                    orderPriority.append(" + ");
+                }
+                caseConditions.append("SUM(CASE WHEN ").append(subConditions).append(" THEN 1 ELSE 0 END) AS ").append(conditionName);
+                whereConditions.append("(").append(subConditions).append(")");
+                matchScore.append("(CASE WHEN ").append(conditionName).append(" > 0 THEN 1 ELSE 0 END)");
+                orderPriority.append(conditionName);
             }
-        }
-        sql.append("(").append(conditions).append(")").append(" ORDER BY NULL");
 
-        return template.queryForList(sql.toString(), params, Long.class);
+            params.put("hue" + i, each.getHue());
+            params.put("saturation" + i, each.getSaturation());
+            params.put("lightness" + i, each.getLightness());
+        }
+        sql.append(caseConditions)
+                .append("""
+                        \nFROM post_color pc
+                        JOIN color c ON pc.color_id = c.color_id
+                        WHERE
+                        """)
+                .append("(").append(whereConditions).append(")")
+                .append("""
+                        \nGROUP BY pc.post_id
+                        )
+                        SELECT post_id,
+                        """)
+                .append("(").append(matchScore).append(") AS match_score")
+                .append("""
+                        \nFROM ColorFilter
+                        ORDER BY match_score DESC,
+                        """)
+                .append(orderPriority).append(" DESC");
+
+        return template.query(sql.toString(), params, colorMatchScoreRowMapper());
     }
 
-    public List<Long> searchByColorCondAND(List<ColorCond> conds) {
-        StringBuilder sql = new StringBuilder("""
-                SELECT pc.post_id
-                FROM post_color pc
-                JOIN color c ON pc.color_id = c.color_id
-                WHERE
-                """);
-        Map<String, Object> params = new HashMap<>();
-        StringBuilder whenConditions = new StringBuilder();
-        StringBuilder caseConditions = new StringBuilder();
-        for (int i = 0; i < conds.size(); i++) {
-            ColorCond each = conds.get(i);
-            StringBuilder subConditions = new StringBuilder();
-
-            if (each.getHue() != null) {
-                subConditions.append("c.hue = :hue").append(i);
-                params.put("hue" + i, each.getHue());
-            }
-            if (each.getSaturation() != null) {
-                if (!subConditions.isEmpty()) subConditions.append(" AND ");
-                subConditions.append("c.saturation = :saturation").append(i);
-                params.put("saturation" + i, each.getSaturation());
-            }
-            if (each.getLightness() != null) {
-                if (!subConditions.isEmpty()) subConditions.append(" AND ");
-                subConditions.append("c.lightness = :lightness").append(i);
-                params.put("lightness" + i, each.getLightness());
-            }
-            if (!subConditions.isEmpty()) {
-                if (i > 0) whenConditions.append(" OR ");
-                whenConditions.append("(").append(subConditions).append(")");
-                caseConditions.append("WHEN ").append(subConditions).append(" THEN 1\n");
-            }
-        }
-        sql.append("(").append(whenConditions).append(")").append("""
-                \nGROUP BY pc.post_id
-                HAVING COUNT(CASE
-                """).append(caseConditions).append("""
-                END) = :colorCount
-                ORDER BY NULL
-                """);
-        params.put("colorCount", conds.size());
-
-        return template.queryForList(sql.toString(), params, Long.class);
+    private RowMapper<ColorMatchScore> colorMatchScoreRowMapper() {
+        return BeanPropertyRowMapper.newInstance(ColorMatchScore.class);
     }
 
 }
