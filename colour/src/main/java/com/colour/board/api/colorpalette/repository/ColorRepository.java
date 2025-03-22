bc895a04458fc3fb082ac9023ee795eeb765f36c
AidenRoh <colohrby@gmail.com>
2025-03-22 23:04:41 +0900

    refactor: move search login into search api

--- colour/src/main/java/com/colour/board/api/colorpalette/repository/ColorRepository.java
+++ colour/src/main/java/com/colour/board/api/colorpalette/repository/ColorRepository.java
@@ -1,9 +1,7 @@
 package com.colour.board.api.colorpalette.repository;
 
-import com.colour.board.api.colorpalette.domain.dto.ColorCond;
 import com.colour.board.api.colorpalette.domain.entity.HexColor;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ColorRepository {
@@ -11,8 +9,6 @@ public interface ColorRepository {
     HexColor save(HexColor color);
 
     Optional<HexColor> findByHex(String hexColor);
-
-    List<HexColor> findByCond(ColorCond cond);
 
     void delete(Long colorId);
 }
