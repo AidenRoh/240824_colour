99926b0d84cf262c3b33fad5faf96c9a20427aaf
AidenRoh <colohrby@gmail.com>
2025-03-23 23:36:53 +0900

    refactor: add locale logic for dynamic language determination

--- colour/src/main/java/com/colour/search/controller/SearchController.java
+++ colour/src/main/java/com/colour/search/controller/SearchController.java
@@ -7,6 +7,7 @@ import com.colour.board.api.post.domain.dto.PostResponseDto;
 import com.colour.search.domain.dto.SearchDto;
 import com.colour.search.domain.enums.SortOption;
 import com.colour.search.facade.SearchFacadeService;
+import jakarta.servlet.ServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static com.colour.search.domain.enums.SortOption.RELEVANCE;
 
@@ -33,9 +35,10 @@ public class SearchController {
     public ResponseEntity<Page<PostResponseDto>> searchPost(@RequestParam(required = false) String q,
                                                             @RequestParam(required = false) String sort,
                                                             @RequestBody SearchDto searchDto,
-                                                            Pageable pageable) throws IOException {
+                                                            Pageable pageable,
+                                                            ServletRequest request) throws IOException {
         Pageable sortPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
-                SortOption.getSort((sort != null) ? sort : RELEVANCE.name()));
+                SortOption.getSort((sort != null) ? sort : RELEVANCE.name().toLowerCase()));
 
         // SearchDto 에 대해 front 는 값이 없더라고 '[]' 로 백엔드로 넘겨야함 (emptyList()) 로 처리하기 위해
         List<HashtagDto> hashtagList = new ArrayList<>(searchDto.getHashtags());
@@ -46,10 +49,13 @@ public class SearchController {
         colorCondList.addAll(searchDto.getColorConds());
 
         if (q != null) {
-            Page<PostResponseDto> postResponseDtos = service.doElasticSearch(q, colorCondList, hashtagList, sortPageable);
+            Locale locale = request.getLocale();
+            Page<PostResponseDto> postResponseDtos =
+                    service.doElasticSearch(q, locale, colorCondList, hashtagList, sortPageable);
             return ResponseEntity.ok(postResponseDtos);
         } else {
-            Page<PostResponseDto> postResponseDtos = service.doFilterSearch(colorCondList, hashtagList, sortPageable);
+            Page<PostResponseDto> postResponseDtos =
+                    service.doFilterSearch(colorCondList, hashtagList, sortPageable);
             return ResponseEntity.ok(postResponseDtos);
         }
 
