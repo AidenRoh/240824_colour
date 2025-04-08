6d00d4905e8e430ffeead72ef898dc233cf07581
AidenRoh <colohrby@gmail.com>
2025-04-08 23:04:22 +0900

    feature: testing minio async client

--- colour/src/main/java/com/colour/files/minio.java
+++ colour/src/main/java/com/colour/files/minio.java
@@ -1,15 +1,13 @@
 package com.colour.files;
 
 import io.minio.MinioAsyncClient;
-import io.minio.MinioClient;
 
 public class minio {
 
     public void example() {
-        MinioClient minioClient = MinioAsyncClient.builder()
+        MinioAsyncClient client = MinioAsyncClient.builder()
                 .endpoint("https://play.min.io")
                 .credentials("1234", "12345")
                 .build();
-
     }
 }
