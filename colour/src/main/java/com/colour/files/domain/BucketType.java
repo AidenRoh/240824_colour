e76bd551740eeb247275265bae2eefb36972ad25
AidenRoh <colohrby@gmail.com>
2025-04-11 21:46:24 +0900

    feature: add bucket type enum to classify media type by extension

---
+++ colour/src/main/java/com/colour/files/domain/BucketType.java
@@ -0,0 +1,28 @@
+package com.colour.files.domain;
+
+import lombok.Getter;
+
+public enum BucketType {
+    IMAGE("colour_image", new String[]{"jpg", "jpeg", "png", "gif"}),
+    VIDEO("colour_video", new String[]{"mp4", "mov", "mkv", "avi"}),
+    THUMBNAIL("colour_thumbnail", new String[]{"thumb"});
+
+    @Getter
+    private final String bucket;
+    private final String[] extensions;
+
+    BucketType(String bucket, String[] extensions) {
+        this.bucket = bucket;
+        this.extensions = extensions;
+    }
+
+    public static String getValidBucket(String extension) {
+        for (BucketType eachType : values()) {
+            for (String eachExtension : eachType.extensions) {
+                if (eachExtension.equalsIgnoreCase(extension)) return eachType.bucket;
+            }
+        }
+        throw new IllegalArgumentException("Invalid extension: " + extension);
+    }
+
+}
