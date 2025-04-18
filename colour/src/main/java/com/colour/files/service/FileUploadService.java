d0b03be6d771deffbdd8cfb25131e63a298c18ad
AidenRoh <colohrby@gmail.com>
2025-04-18 23:37:05 +0900

    refactor: add constructor

--- colour/src/main/java/com/colour/files/service/FileUploadService.java
+++ colour/src/main/java/com/colour/files/service/FileUploadService.java
@@ -1,14 +1,20 @@
 package com.colour.files.service;
 
 import com.colour.files.repository.MinIoRepository;
+import io.minio.http.Method;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileUploadService {
 
-    MinIoRepository repository;
+    private final MinIoRepository repository;
+
+    public FileUploadService(MinIoRepository repository) {
+        this.repository = repository;
+    }
 
     public String getPresignedUrl(String fileName, String fileType) {
-        return repository.IssuePresignedUrl(fileName, fileType);
+        return repository.IssuePresignedUrl(fileName, fileType, Method.PUT);
     }
+
 }
