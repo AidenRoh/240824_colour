5e808e6acd264e5107998f2b58309a6ec89f90e7
AidenRoh <colohrby@gmail.com>
2025-04-19 23:03:17 +0900

    refactor: add test log for language detector library

--- colour/src/main/java/com/colour/ColourApplication.java
+++ colour/src/main/java/com/colour/ColourApplication.java
@@ -1,13 +1,33 @@
 package com.colour;
 
+import org.apache.tika.language.detect.LanguageDetector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import java.util.ServiceLoader;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ColourApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ColourApplication.class, args);
+
+        ServiceLoader<LanguageDetector> loader = ServiceLoader.load(LanguageDetector.class);
+
+        if (!loader.iterator().hasNext()) {
+            System.out.println("❌ No LanguageDetector found! Check tika-langdetect dependency.");
+        } else {
+            System.out.println("✅ LanguageDetector is available.");
+            for (LanguageDetector detector : loader) {
+                System.out.println("Detected Detector: " + detector.getClass().getName());
+            }
+        }
+
+
     }
 
 }
